885046a85a91909e86bdee6f86f55b94fb55b42c
bartek <pingueue@gmail.com>
2024-10-18 16:01:34 +0200

    wafafaw

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,15 @@
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
-export default function Footer({className}: {className?: string}) {
+export default function Footer({ className }: { className?: string }) {
     return (
-        <footer className={cn(className,"flex p-10 gap-6 flex-wrap items-center justify-center")}>
-            <div>future link1</div>
-            <div>future link2</div>
-            <div>future link3</div>
-            <div>future link4</div>
+        <footer className={cn(className, "flex p-10 gap-6 flex-col border-t")}>
+            <div><Link href="/bartek">
+                link 1 </Link></div>
+            <div><Link href="/bartek">
+            link 2 </Link></div>
+            <div><Link href="/bartek">
+            link 3 </Link></div>
         </footer>
     )
 }
